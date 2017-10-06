f72e4163ed364ea0fc564be83481be172de0fa0a
HarishAtGitHub <hkayaroh@asu.edu>
2017-10-06 08:46:51 +0000

    upgrading to python 3.6

--- scanfaces.py
+++ scanfaces.py
@@ -2,12 +2,29 @@ import cv2
 import os
 import argparse
 import base64
-from PIL import Image
-from StringIO import StringIO
 import numpy as np
+print(np.__version__)
+print('**************************')
+
+
+def get_face_info(img_base64):
+    image_match_url = 'https://h2h2c0e7p9.execute-api.us-west-2.amazonaws.com/beta/imageinfo'
+    headers = {'Content-Type' : 'application/json',
+               'x-api-key' : 'h4ANNxg5AA5PpYMxu3QZg7t8C9St6KKI9rZPmrQT'}
+    request_payload = {
+        'image':'',
+    }
+    request_payload['image'] = img_base64.decode('utf-8')
+    import requests
+    import json
+    response = requests.post(image_match_url,
+                             data=json.dumps(request_payload),
+                             headers=headers)
+    print(response.json())
+    print(type(response.json()))
 
 def readb64(base64_string):
-    nparr = np.fromstring(base64_string.decode('base64'), np.uint8)
+    nparr = np.fromstring(base64.b64decode(base64_string), np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return img
 
@@ -15,7 +32,6 @@ def get_faces(base64_string):
     face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_eye.xml')
     img = readb64(base64_string)
-    #print(img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.2, 5)
 
@@ -39,11 +55,22 @@ def get_info(img_base64, outputdir):
         img_base64 = base64.b64encode(img_binary)
     faces = get_faces(img_base64)
     num_faces = 0
+    import sys
+    print(sys.version_info)
+    print(np.__version__)
+    print('**************************')
+
     for face in faces:
         face_filename = os.path.join(outputdir, "face{}.png".format(num_faces))
         cv2.imwrite(face_filename, face)
         num_faces += 1
+
+        #img = face.tobytes()
+        #print(img)
+        #face = base64.b64encode(img)
+        #get_face_info(face)
     
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract faces from photos.')
     parser.add_argument('outputdir', type=str, help='Output directory for faces')
